40114025637996589ef788d8309ac6382c85e126
bakkerjoeri <bakkerjoeri@gmail.com>
2022-03-08 12:15:10 +0100

    feat(nvim): adds gb shortcut

--- nvim/lua/user/plugins/bufferline.lua
+++ nvim/lua/user/plugins/bufferline.lua
@@ -1,3 +1,5 @@
+local keymap = require('lib.keymap')
+
 vim.cmd [[highlight BufferlineOffset guifg = '#80a0ff' guibg = '#21222C']]
 
 require('bufferline').setup({
@@ -10,6 +12,9 @@ require('bufferline').setup({
 			text_align = 'left',
 		},
 		separator_style = 'thin',
+		show_buffer_close_icons = false,
 		show_close_icon = false,
 	},
 })
+
+keymap('n', 'gb', ':BufferLinePick<CR>')
