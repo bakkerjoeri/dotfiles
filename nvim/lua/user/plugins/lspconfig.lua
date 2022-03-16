1f144df712d6bfe4f7a11a8d1b3703bce199ff45
bakkerjoeri <bakkerjoeri@gmail.com>
2022-03-16 17:25:29 +0100

    fix(nvim): runs svelte language server correctly

--- nvim/lua/user/plugins/lspconfig.lua
+++ nvim/lua/user/plugins/lspconfig.lua
@@ -27,24 +27,19 @@ for _, lsp in pairs({
 	'eslint',
 	'html',
 	'jsonls',
+	'svelte',
+	'tsserver',
 }) do
 	lspconfig[lsp].setup({
-		on_attach = on_attach,
+		on_attach = function(client, bufnr)
+			client.resolved_capabilities.document_formatting = false
+			client.resolved_capabilities.document_range_formatting = false
+			on_attach(client, bufnr)
+		end,
 		capabilities = capabilities,
 		flags = { debounce_text_changes = 150 },
 	})
 end
-
-lspconfig.tsserver.setup({
-	on_attach = function(client, bufnr)
-		client.resolved_capabilities.document_formatting = false
-		client.resolved_capabilities.document_range_formatting = false
-		on_attach(client, bufnr)
-	end,
-	capabilities = capabilities,
-	flags = { debounce_text_changes = 150 },
-	root_dir = lspconfig.util.root_pattern("tsconfig.json", "package.json", ".git"),
-})
 
 null_ls.setup({
 	on_attach = function(client, bufnr)
