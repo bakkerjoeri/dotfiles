58452ef736072c64defca4b36126e7f63ae7661b
bakkerjoeri <bakkerjoeri@gmail.com>
2022-04-19 12:29:42 +0200

    feat: improves keymapping for files

--- nvim/lua/user/plugins/telescope.lua
+++ nvim/lua/user/plugins/telescope.lua
@@ -2,9 +2,9 @@ local telescope = require 'telescope'
 local keymap = require 'lib.keymap'
 
 keymap('n', '<leader>f', [[<cmd>lua require('telescope.builtin').find_files()<CR>]])
-keymap('n', '<leader>F', [[<cmd>lua require('telescope.builtin').find_files({ no_ignore = true, prompt_title = 'All Files' })<CR>]])
+keymap('n', '<leader><C-f>', [[<cmd>lua require('telescope.builtin').find_files({ no_ignore = true, prompt_title = 'All Files' })<CR>]])
 keymap('n', '<leader>b', [[<cmd>lua require('telescope.builtin').buffers()<CR>]])
-keymap('n', '<leader>r', [[<cmd>lua require('telescope').extensions.live_grep_raw.live_grep_raw()<CR>]])
+keymap('n', '<leader>F', [[<cmd>lua require('telescope').extensions.live_grep_raw.live_grep_raw()<CR>]])
 
 telescope.setup {
 	defaults = {
