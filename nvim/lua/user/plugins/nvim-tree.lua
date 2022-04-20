d6dbef04ef8ef47211e0c2270d2842c0e2f7bbb9
bakkerjoeri <bakkerjoeri@gmail.com>
2022-04-21 00:38:39 +0200

    fix(nvim): updates to new nvim tree setup format

--- nvim/lua/user/plugins/nvim-tree.lua
+++ nvim/lua/user/plugins/nvim-tree.lua
@@ -1,10 +1,14 @@
-vim.g.nvim_tree_indent_markers = 1
 vim.g.nvim_tree_highlight_opened_files = 1
 vim.g.nvim_tree_group_empty = 1
 
 require('nvim-tree').setup {
 	git = {
 		ignore = false,
+	},
+	renderer = {
+		indent_markers = {
+			enable = true,
+		},
 	},
 }
 
