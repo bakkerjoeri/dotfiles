88628e195366dd52828a5dbd9d2cf0ebd4315a94
bakkerjoeri <bakkerjoeri@gmail.com>
2022-09-12 15:05:19 +0200

    fixes capabilities

--- nvim/lua/user/plugins/lspconfig.lua
+++ nvim/lua/user/plugins/lspconfig.lua
@@ -31,45 +31,35 @@ for _, lsp in pairs({
 	'tsserver',
 }) do
 	lspconfig[lsp].setup({
-		on_attach = function(client, bufnr)
-			client.server_capabilities.documentFormattingProvider = false
-			client.server_capabilities.documentRangeFormattingProvider = false
-			on_attach(client, bufnr)
-		end,
 		capabilities = capabilities,
 		flags = { debounce_text_changes = 150 },
+		on_attach = function(client, bufnr)
+			client.resolved_capabilities.document_formatting = false
+			client.resolved_capabilities.document_range_formatting = false
+			on_attach(client, bufnr)
+		end,
 	})
 end
 
 null_ls.setup({
-	on_attach = function(client, bufnr)
-		if client.server_capabilities.documentFormattingProvider then
-			vim.cmd([[
-				augroup LspFormatting
-					autocmd! * <buffer>
-					autocmd BufWritePre <buffer> lua vim.lsp.buf.format()
-				augroup END
-			]])
-		end
-
-		on_attach(client, bufnr)
-	end,
 	capabilities = capabilities,
 	flags = { debounce_text_changes = 150 },
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					vim.lsp.buf.formatting_sync()
+				end,
+			})
+		end
+	end,
 	sources = {
 		null_ls.builtins.formatting.prettier.with({
 			only_local = "node_modules/.bin",
-			extra_filetypes = {
-				"javascript",
-				"javascriptreact",
-				"typescript",
-				"typescriptreact",
-				"svelte",
-				"css",
-				"html",
-				"json",
-				"markdown",
-			},
+			extra_filetypes = { "svelte" },
 		}),
 	},
 })
