d24178e2211f1e9fd6a6e3ff5d0be70511c59f2d
bakkerjoeri <bakkerjoeri@gmail.com>
2024-01-08 12:20:46 +0100

    highlight current line

--- nvim/init.lua
+++ nvim/init.lua
@@ -68,6 +68,7 @@ vim.o.tabstop = 4
 vim.o.signcolumn = 'yes:2'
 vim.o.relativenumber = true
 vim.o.termguicolors = true
+vim.o.cursorline = true
 vim.o.clipboard = 'unnamedplus'
 vim.o.title = true
 vim.o.ignorecase = true
