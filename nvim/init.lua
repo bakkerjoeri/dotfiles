7a9a0b336a6ce6942be5c5e4281173fb97e3b974
bakkerjoeri <bakkerjoeri@gmail.com>
2024-01-08 12:21:03 +0100

    improves cmp config

--- nvim/init.lua
+++ nvim/init.lua
@@ -411,7 +411,6 @@ require('lazy').setup({
 	{
 		-- Autocompletion
 		'hrsh7th/nvim-cmp',
-		lazy = false,
 		dependencies = {
 			'hrsh7th/cmp-nvim-lsp',
 			'L3MON4D3/LuaSnip',
@@ -423,112 +422,9 @@ require('lazy').setup({
 			'hrsh7th/cmp-nvim-lsp-signature-help',
 		},
 		config = function()
-			local cmp = require('cmp')
-			local luasnip = require('luasnip')
-			local cmp_mapping = require "cmp.config.mapping"
-			local status_cmp_ok, cmp_types = pcall(require, "cmp.types.cmp")
-			if not status_cmp_ok then
-				return
-			end
-			local ConfirmBehavior = cmp_types.ConfirmBehavior
-			local SelectBehavior = cmp_types.SelectBehavior
-
-			cmp.setup({
-				snippet = {
-					expand = function(args)
-						luasnip.lsp_expand(args.body)
-					end,
-				},
-				mapping = cmp_mapping.preset.insert {
-					["<C-k>"] = cmp_mapping(cmp_mapping.select_prev_item(), { "i", "c" }),
-					["<C-j>"] = cmp_mapping(cmp_mapping.select_next_item(), { "i", "c" }),
-					["<Down>"] = cmp_mapping(cmp_mapping.select_next_item { behavior = SelectBehavior.Select }, { "i" }),
-					["<Up>"] = cmp_mapping(cmp_mapping.select_prev_item { behavior = SelectBehavior.Select }, { "i" }),
-					["<C-d>"] = cmp_mapping.scroll_docs(-4),
-					["<C-f>"] = cmp_mapping.scroll_docs(4),
-					["<C-y>"] = cmp_mapping {
-						i = cmp_mapping.confirm { behavior = ConfirmBehavior.Replace, select = false },
-						c = function(fallback)
-							if cmp.visible() then
-								cmp.confirm { behavior = ConfirmBehavior.Replace, select = false }
-							else
-								fallback()
-							end
-						end,
-					},
-					["<Tab>"] = cmp_mapping(function(fallback)
-						if cmp.visible() then
-							cmp.select_next_item()
-						elseif luasnip.expand_or_locally_jumpable() then
-							luasnip.expand_or_jump()
-						elseif jumpable(1) then
-							luasnip.jump(1)
-						elseif has_words_before() then
-							-- cmp.complete()
-							fallback()
-						else
-							fallback()
-						end
-					end, { "i", "s" }),
-					["<S-Tab>"] = cmp_mapping(function(fallback)
-						if cmp.visible() then
-							cmp.select_prev_item()
-						elseif luasnip.jumpable(-1) then
-							luasnip.jump(-1)
-						else
-							fallback()
-						end
-					end, { "i", "s" }),
-					["<C-Space>"] = cmp_mapping.complete(),
-					["<C-e>"] = cmp_mapping.abort(),
-					["<CR>"] = cmp_mapping(function(fallback)
-						if cmp.visible() then
-							local confirm_opts = vim.deepcopy(lvim.builtin.cmp.confirm_opts) -- avoid mutating the original opts below
-							local is_insert_mode = function()
-								return vim.api.nvim_get_mode().mode:sub(1, 1) == "i"
-							end
-							if is_insert_mode() then -- prevent overwriting brackets
-								confirm_opts.behavior = ConfirmBehavior.Insert
-							end
-							local entry = cmp.get_selected_entry()
-							local is_copilot = entry and entry.source.name == "copilot"
-							if is_copilot then
-								confirm_opts.behavior = ConfirmBehavior.Replace
-								confirm_opts.select = true
-							end
-							if cmp.confirm(confirm_opts) then
-								return -- success, exit early
-							end
-						end
-						fallback() -- if not exited early, always fallback
-					end),
-				},
-				sources = {
-					{ name = 'nvim_lsp' },
-					{ name = 'nvim_lsp_signature_help' },
-					{ name = 'nvim_lua' },
-					{ name = 'luasnip' },
-					{ name = 'calc' },
-					{ name = 'emoji' },
-					{ name = 'treesitter' },
-					{ name = 'path' },
-					{ name = 'buffer' },
-				},
-				formatting = {
-					source_names = {
-						nvim_lsp = "(LSP)",
-						nvim_lsp_signature_help = "(LSP)",
-						nvim_lua = "(LUA)",
-						emoji = "(Emoji)",
-						path = "(Path)",
-						calc = "(Calc)",
-						luasnip = "(Snippet)",
-						buffer = "(Buffer)",
-						treesitter = "(TreeSitter)",
-					},
-				}
-			})
+			require('plugins/cmp').setup()
 		end,
+		event = { "InsertEnter", "CmdlineEnter" },
 	},
 	{
 		'akinsho/toggleterm.nvim',
